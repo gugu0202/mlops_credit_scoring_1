714b97f07bda9d7b0ec24d7afe3a3047c98a4af1
gugu0202 <gula.r.bikl@gmail.com>
2026-02-16 03:23:04 +0300

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,3 @@
+DATA_PATH = "data/UCI_Credit_Card.csv"
+EXPERIMENT_NAME = "Credit Risk Prediction"
+TRACKING_URI = "file:/./mlruns/"
---
+++ src/utils.py
@@ -0,0 +1,28 @@
+
+import os
+import shutil
+import mlflow
+import mlflow.sklearn
+
+def init_mlflow():
+    """
+    Инициализирует среду MLFlow и создаёт каталог для экспериментов.
+    """
+    tracking_uri = "file:/./mlruns/"
+    mlflow.set_tracking_uri(tracking_uri)
+    mlflow.set_experiment("Credit Risk Prediction")
+
+def log_experiment(model_name, model, metrics, artifacts=[]):
+    """
+    Логирует эксперимент в MLFlow.
+    :param model_name: Название модели.
+    :param model: Сама модель.
+    :param metrics: Словарь метрик.
+    :param artifacts: Дополнительные артефакты (графики и т.д.).
+    """
+    with mlflow.start_run(run_name=model_name):
+        mlflow.log_params(model.get_params())
+        mlflow.log_metrics(metrics)
+        for artifact in artifacts:
+            mlflow.log_artifact(artifact)
+        mlflow.sklearn.log_model(model, artifact_path="model")
